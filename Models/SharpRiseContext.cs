02a29c8b450f4920aa10b0dea68574510dd8a4aa
Freakovv <misha.simanovich@mail.ru>
2025-06-06 22:43:51 +0300

    Tweak: Added cascade delete

--- Models/SharpRiseContext.cs
+++ Models/SharpRiseContext.cs
@@ -21,28 +21,11 @@ public partial class SharpRiseContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Group>(entity =>
+        modelBuilder.Entity<Teacher>(entity =>
         {
-            entity.HasIndex(e => e.TeacherId, "IX_Groups_TeacherId");
-
-            entity.Property(e => e.Description).HasMaxLength(200);
-            entity.Property(e => e.InviteCode).HasMaxLength(9);
-            entity.Property(e => e.Name).HasMaxLength(20);
-            entity.Property(e => e.TeacherUsername).HasMaxLength(30);
-
-            entity.HasOne(d => d.Teacher).WithMany(p => p.Groups).HasForeignKey(d => d.TeacherId);
-
-            entity.HasMany(d => d.Students).WithMany(p => p.Groups)
-                .UsingEntity<Dictionary<string, object>>(
-                    "GroupStudent",
-                    r => r.HasOne<Student>().WithMany().HasForeignKey("StudentsId"),
-                    l => l.HasOne<Group>().WithMany().HasForeignKey("GroupsId"),
-                    j =>
-                    {
-                        j.HasKey("GroupsId", "StudentsId");
-                        j.ToTable("GroupStudent");
-                        j.HasIndex(new[] { "StudentsId" }, "IX_GroupStudent_StudentsId");
-                    });
+            entity.Property(e => e.Email).HasMaxLength(254);
+            entity.Property(e => e.Password).HasMaxLength(64);
+            entity.Property(e => e.Username).HasMaxLength(30);
         });
 
         modelBuilder.Entity<Student>(entity =>
@@ -52,11 +35,36 @@ public partial class SharpRiseContext : DbContext
             entity.Property(e => e.Username).HasMaxLength(30);
         });
 
-        modelBuilder.Entity<Teacher>(entity =>
+        modelBuilder.Entity<Group>(entity =>
         {
-            entity.Property(e => e.Email).HasMaxLength(254);
-            entity.Property(e => e.Password).HasMaxLength(64);
-            entity.Property(e => e.Username).HasMaxLength(30);
+            entity.HasIndex(e => e.TeacherId, "IX_Groups_TeacherId");
+
+            entity.Property(e => e.Description).HasMaxLength(200);
+            entity.Property(e => e.InviteCode).HasMaxLength(9);
+            entity.Property(e => e.Name).HasMaxLength(20);
+            entity.Property(e => e.TeacherUsername).HasMaxLength(30);
+
+            entity.HasOne(d => d.Teacher)
+                  .WithMany(p => p.Groups)
+                  .HasForeignKey(d => d.TeacherId)
+                  .OnDelete(DeleteBehavior.Cascade); 
+
+            entity.HasMany(d => d.Students)
+                  .WithMany(p => p.Groups)
+                  .UsingEntity<Dictionary<string, object>>(
+                      "GroupStudent",
+                      r => r.HasOne<Student>().WithMany()
+                            .HasForeignKey("StudentsId")
+                            .OnDelete(DeleteBehavior.Cascade), 
+                      l => l.HasOne<Group>().WithMany()
+                            .HasForeignKey("GroupsId")
+                            .OnDelete(DeleteBehavior.Cascade),
+                      j =>
+                      {
+                          j.HasKey("GroupsId", "StudentsId");
+                          j.ToTable("GroupStudent");
+                          j.HasIndex(new[] { "StudentsId" }, "IX_GroupStudent_StudentsId");
+                      });
         });
 
         OnModelCreatingPartial(modelBuilder);
