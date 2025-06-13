724781a91015c03c916d0a7b5b228fca18681e51
Freakovv <misha.simanovich@mail.ru>
2025-06-14 02:06:25 +0300

    SR-2: [Refactor] Moving tailwind config

---
+++ wwwroot/js/tailwind.config.js
@@ -0,0 +1,22 @@
+window.tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#9B4F96",
+        secondary: "#6D4C9F",
+      },
+      borderRadius: {
+        none: "0px",
+        sm: "4px",
+        DEFAULT: "8px",
+        md: "12px",
+        lg: "16px",
+        xl: "20px",
+        "2xl": "24px",
+        "3xl": "32px",
+        full: "9999px",
+        button: "8px",
+      },
+    },
+  },
+};
