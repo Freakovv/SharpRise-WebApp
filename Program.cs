8450ab53fee152f51b050e2ba393144332b43bd6
Freakovv <misha.simanovich@mail.ru>
2025-06-14 11:45:27 +0300

    SR-2: [Modified] Coockie options

--- Program.cs
+++ Program.cs
@@ -1,14 +1,38 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using SharpRise_WebApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Добавляем сервисы в контейнер
 builder.Services.AddDbContext<SharpRiseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Добавляем сервисы аутентификации
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = "SharpRise.Auth";
+        options.LoginPath = "/Account/Login";
+        options.LogoutPath = "/Account/Logout";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+        options.ExpireTimeSpan = TimeSpan.FromDays(30); // Куки будут храниться 30 дней
+        options.SlidingExpiration = true; // Обновлять срок действия при каждом запросе
+    });
+
+// Добавляем политику cookie
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.MinimumSameSitePolicy = SameSiteMode.Lax;
+    options.HttpOnly = Microsoft.AspNetCore.CookiePolicy.HttpOnlyPolicy.Always;
+    options.Secure = CookieSecurePolicy.Always;
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Настройка конвейера HTTP-запросов
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -17,7 +41,11 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseCookiePolicy(); // Добавляем политику cookie
 app.UseRouting();
+
+// Важно: UseAuthentication ДО UseAuthorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
