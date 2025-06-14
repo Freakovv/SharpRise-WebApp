6c629b808166c88bdf4d6649be2fd8edac76fc25
Freakovv <misha.simanovich@mail.ru>
2025-06-14 11:47:47 +0300

    SR-2: [Add] Register and Login models

--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -8,23 +8,23 @@ namespace SharpRise_WebApp.Controllers
         [Required(ErrorMessage = "Поле обязательно для заполнения")]
         [EmailAddress(ErrorMessage = "Некорректный email адрес")]
         [Display(Name = "Email")]
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
         [Required(ErrorMessage = "Поле обязательно для заполнения")]
         [StringLength(30, MinimumLength = 3, ErrorMessage = "Имя пользователя должно быть от 3 до 30 символов")]
         [Display(Name = "Имя пользователя")]
-        public string Username { get; set; }
+        public required string Username { get; set; }
 
         [Required(ErrorMessage = "Поле обязательно для заполнения")]
         [StringLength(64, MinimumLength = 8, ErrorMessage = "Пароль должен быть от 8 до 64 символов")]
         [DataType(DataType.Password)]
         [Display(Name = "Пароль")]
-        public string Password { get; set; }
+        public required string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Подтверждение пароля")]
         [Compare("Password", ErrorMessage = "Пароли не совпадают")]
-        public string ConfirmPassword { get; set; }
+        public required string ConfirmPassword { get; set; }
 
         [Required(ErrorMessage = "Необходимо принять условия использования")]
         [Range(typeof(bool), "true", "true", ErrorMessage = "Необходимо принять условия использования")]
